bcb63cfff268c923f74addc2e1691ff6e5532865
Erich <stark.erich72@gmail.com>
2014-04-12 23:59:54 +0200

    Navigation driver basics

---
+++ src/com/erichstark/pedometer/adapter/NavigationDrawerListAdapter.java
@@ -0,0 +1,76 @@
+package com.erichstark.pedometer.adapter;
+
+import java.util.ArrayList;
+
+import com.erichstark.pedometer.R;
+import com.erichstark.pedometer.model.NavigationDrawerItem;
+
+
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class NavigationDrawerListAdapter extends BaseAdapter {
+
+	private Context context;
+	private ArrayList<NavigationDrawerItem> navItems;
+
+	public NavigationDrawerListAdapter(Context context,
+			ArrayList<NavigationDrawerItem> navItems) {
+		super();
+		this.context = context;
+		this.navItems = navItems;
+	}
+
+	// return count of items
+	@Override
+	public int getCount() {
+		return navItems.size();
+	}
+
+	// return object with that position
+	@Override
+	public Object getItem(int position) {
+		return navItems.get(position);
+	}
+
+	// return position of item in list
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.drawer_list_item, null);
+		}
+		
+		ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+		TextView tvTitle = (TextView) convertView.findViewById(R.id.title);
+		TextView tvCount = (TextView) convertView.findViewById(R.id.counter);
+		
+		imgIcon.setImageResource(navItems.get(position).getIcon());
+		tvTitle.setText(navItems.get(position).getTitle());
+		
+		// check if count must be visible or not and set number
+		if (navItems.get(position).isCounterVisible()) {
+			tvCount.setText(navItems.get(position).getCount());
+		} else {
+			tvCount.setVisibility(View.GONE);
+		}
+		
+		return convertView;
+	}
+
+}
---
+++ src/com/erichstark/pedometer/model/NavigationDrawerItem.java
@@ -0,0 +1,51 @@
+package com.erichstark.pedometer.model;
+
+public class NavigationDrawerItem {
+
+	private int icon;
+	private String title;
+	private String count;
+	private boolean isCounterVisible = false;
+	
+	public NavigationDrawerItem() {
+	}
+	
+	public NavigationDrawerItem(int icon, String title){
+        this.icon = icon;
+        this.title = title;
+    }
+	
+	public NavigationDrawerItem(int icon, String title, String count,
+			boolean isCounterVisible) {
+		this.icon = icon;
+		this.title = title;
+		this.count = count;
+		this.isCounterVisible = isCounterVisible;
+	}
+	
+	public int getIcon() {
+		return icon;
+	}
+	public void setIcon(int icon) {
+		this.icon = icon;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	public boolean isCounterVisible() {
+		return isCounterVisible;
+	}
+	public void setCounterVisible(boolean isCounterVisible) {
+		this.isCounterVisible = isCounterVisible;
+	}
+	
+}
