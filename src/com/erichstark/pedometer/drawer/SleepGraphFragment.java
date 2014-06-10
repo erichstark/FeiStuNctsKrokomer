2b277c6852f0e699884528855e0b24c51c4c6420
Erich <stark.erich72@gmail.com>
2014-06-11 01:45:44 +0200

    New app icons and class diagrams

--- src/com/erichstark/pedometer/drawer/SleepGraphFragment.java
+++ src/com/erichstark/pedometer/drawer/SleepGraphFragment.java
@@ -1,7 +1,6 @@
 package com.erichstark.pedometer.drawer;
 
 import java.text.Format;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
